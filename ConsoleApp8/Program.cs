5f42c937092a863281aa54292e96b5d816a49677
pinank48 <56752593+pinank48@users.noreply.github.com>
2019-11-10 20:26:15 +0530

    added

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
